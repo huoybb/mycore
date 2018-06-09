3e39061e3da18e986f09db5f903a88a440e4cdec
huoybb <zhaobing024@gmail.com>
2018-06-09 18:36:53 +0800

    更新 1、调整 myCliApp的load函数，便于更加通用，灵活配置命令

--- src/myCliApp.php
+++ src/myCliApp.php
@@ -11,20 +11,15 @@ namespace huoybb\core;
 class myCliApp extends \Symfony\Component\Console\Application
 {
 
-
     /**
-     * myCliApp constructor.
+     * 利用这个加载某一个目录下的命令，方便操作
+     * @param null $path
      */
-    public function __construct($name = 'UNKNOWN', $version = 'UNKNOWN')
+    public function load($path = null)
     {
-        parent::__construct($name,$version);
-        $this->load();
-    }
-
-    public function load()
-    {
+        $path = $path ?? APP_PATH.'/commands';
         $finder = new \Symfony\Component\Finder\Finder();
-        $finder->files()->in(APP_PATH.'/commands');//这里的目录定义是一个问题，需要后续解决，怎么能够识别出来
+        $finder->files()->in($path);//这里的目录定义是一个问题，需要后续解决，怎么能够识别出来
         foreach ($finder as $file){
             $command = '\\Commands\\'.preg_replace('|.php$|','',$file->getBasename());
             $this->add(new $command);
